151ce003078a556aad23406c575db19b5c776163
Vaidik2473 <76964059+Vaidik2473@users.noreply.github.com>
2023-05-04 16:36:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.static('views'))
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
