82f91fb566d5c3bd6f1a6ceb4879cc0483bc93af
Vaidik2473 <76964059+Vaidik2473@users.noreply.github.com>
2023-05-04 16:38:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-    res.render("index")
+    res.send("index")
 })
 
 app.get("/signin", function(req,res){
